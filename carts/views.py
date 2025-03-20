191ba6b317555e09f28895bb157e2c701259a339
Ujjwal Sharma <sharmaujjwal5705@gmail.com>
2025-03-21 02:14:28 +0530

    product_variation_intro

--- carts/views.py
+++ carts/views.py
@@ -13,6 +13,10 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
+    color = request.GET['color']
+    size = request.GET['size']
+
+    return HttpResponse({color, size})
     product = Product.objects.get(id=product_id)                            #get the product
 
     try:
