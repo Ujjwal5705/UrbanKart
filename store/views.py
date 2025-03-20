4e1496bc16f4bbb88cfd0ad18a0ba2aa43ae1245
Ujjwal Sharma <sharmaujjwal5705@gmail.com>
2025-03-20 22:57:04 +0530

    Search_functionality

--- store/views.py
+++ store/views.py
@@ -3,8 +3,8 @@ from .models import Product
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
-
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 
@@ -42,3 +42,17 @@ def product_detail(request, category_slug=None,  product_slug=None):
     }
 
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword)).order_by('-created_date')
+            context = {
+                "products": products,
+                "prod_count": products.count(),
+            }
+            return render(request, 'store/store.html', context)
+        else:
+            return redirect('store')
