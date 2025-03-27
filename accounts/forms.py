dbe6df5b66b5b54cbc93dc643781c0f16f7beed4
Ujjwal Sharma <sharmaujjwal5705@gmail.com>
2025-03-28 00:37:32 +0530

    Added __init__ function in RegisterForm to add placeholder and class in every field, super() is used for calling parent class constructor for proper form initialisation

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,20 @@ from django import forms
 from .models import Account
 
 class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'email', 'phone_number', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email Address'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
+        self.fields['password'].widget.attrs['placeholder'] = 'Create Password'
+        self.fields['confirm_password'].widget.attrs['placeholder'] = 'Confirm Password'
+
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
