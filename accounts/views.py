2bfac0886820ac8f79c6e74b0549534ff21914d0
Ujjwal Sharma <sharmaujjwal5705@gmail.com>
2025-04-02 01:47:29 +0530

    Dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -63,12 +63,18 @@ def login(request):
 
         if user is not None:
             auth.login(request, user)
-            return redirect('home')
+            messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
         else:
             messages.error(request, 'Invalid Login Credentials')
             return redirect('login')
 
     return render(request, 'accounts/login.html')
+
+
+@login_required(login_url = 'login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
 
 
 @login_required(login_url = 'login')
