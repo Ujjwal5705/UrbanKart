21307202caac47d81a72310e556b7c6693bffdaf
Ujjwal Sharma <sharmaujjwal5705@gmail.com>
2025-04-16 00:44:00 +0530

    Modified admin panel of class Order

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,12 @@ from .models import Order, Payment, OrderProduct
 
 # Register your models here.
 
-admin.site.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["order_number", "full_name", "email", "phone_number", "full_address", "order_total", "tax", "status", "created_at", "is_ordered"]
+    list_filter = ["status", "is_ordered"]
+    search_fields = ["order_number", "full_name", "email", "phone_number"]
+    list_per_page = 20
+
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Payment)
 admin.site.register(OrderProduct)
