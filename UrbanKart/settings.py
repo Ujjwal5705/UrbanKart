18a60c42818fe19acf5c1c813879207f7694803b
Ujjwal Sharma <sharmaujjwal5705@gmail.com>
2025-05-23 22:31:59 +0530

    Session Timeout bug fix

--- UrbanKart/settings.py
+++ UrbanKart/settings.py
@@ -62,9 +62,9 @@ MIDDLEWARE = [
     'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
 
-SESSION_EXPIRE_SECONDS = 600
+SESSION_EXPIRE_SECONDS = 30
 SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
-SESSION_TIMEOUT_REDIRECT = 'account/login/'
+SESSION_TIMEOUT_REDIRECT = '/account/login'
 
 
 ROOT_URLCONF = "UrbanKart.urls"
